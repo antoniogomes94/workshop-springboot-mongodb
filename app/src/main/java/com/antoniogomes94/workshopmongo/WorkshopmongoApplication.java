8c978cbf9a75cce5bc69fc53a5368222de427f0d
antoniogomes94 <antoniogomes313@outlook.com>
2023-12-01 20:59:15 -0300

    Entity User e REST funcionando

--- app/src/main/java/com/antoniogomes94/workshopmongo/WorkshopmongoApplication.java
+++ app/src/main/java/com/antoniogomes94/workshopmongo/WorkshopmongoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkshopmongoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
