a12c8682840a9c697f4fb9e050f6e73430d56628
antoniogomes94 <antoniogomes313@outlook.com>
2023-12-01 21:53:13 -0300

    Obtendo um usuario por id

--- app/src/main/java/com/antoniogomes94/workshopmongo/service/UserService.java
+++ app/src/main/java/com/antoniogomes94/workshopmongo/service/UserService.java
@@ -1,11 +1,13 @@
 package com.antoniogomes94.workshopmongo.service;
 
 import com.antoniogomes94.workshopmongo.domain.User;
+import com.antoniogomes94.workshopmongo.exception.ObjectNotFoundException;
 import com.antoniogomes94.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
