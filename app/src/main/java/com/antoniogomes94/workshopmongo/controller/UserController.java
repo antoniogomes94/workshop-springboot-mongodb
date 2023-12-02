d522b73fe06267debf751316532394e495111cb7
antoniogomes94 <antoniogomes313@outlook.com>
2023-12-01 21:35:59 -0300

    Usando padrao DTO para retornar usuarios

--- app/src/main/java/com/antoniogomes94/workshopmongo/controller/UserController.java
+++ app/src/main/java/com/antoniogomes94/workshopmongo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.antoniogomes94.workshopmongo.controller;
 
 import com.antoniogomes94.workshopmongo.domain.User;
+import com.antoniogomes94.workshopmongo.dto.UserDTO;
 import com.antoniogomes94.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -19,9 +21,10 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
